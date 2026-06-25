3c345af1e089b88b59fcf5c0a557f0680244f902
Dmitriy <dmitriy@example.com>
2026-06-25 17:06:36 +0500

    T29: Add build:watch script and export types from public-api

--- projects/core/src/lib/public-api.ts
+++ projects/core/src/lib/public-api.ts
@@ -5,3 +5,4 @@ export * from './crud/crud.service';
 export * from './data-patch/data-patch.service';
 export * from './schema-renderer/schema-renderer.service';
 export * from './theme/theme.service';
+export * from './types';
